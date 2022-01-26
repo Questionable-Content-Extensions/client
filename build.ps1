f86cbfc26ac0c40c1921b6aaebade2c80b6daace
Alexander Krivács Schrøder <alexschrod@gmail.com>
2022-01-26 07:37:41 +0100

    Run `dist` with the build script

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 New-Item dist -ItemType Directory -Force
 
 docker build . -t qcext-client
-docker run --mount "type=bind,source=${PWD}\\dist,target=/app/dist" qcext-client
+docker run --mount "type=bind,source=${PWD}\\dist,target=/app/dist" -e grunt_command=dist qcext-client
 
 Move-Item .\dist\package-lock.json . -Force
