88bc92de712fec92fb4f2318e21eb9204991549c
Alexander Krivács Schrøder <alexschrod@gmail.com>
2023-08-13 11:25:10 +0200

    test: add story for `ModalDialog`

---
+++ src/components/Modals/ModalDialog.stories.tsx
@@ -0,0 +1,85 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import ModalDialog from './ModalDialog'
+
+export default {
+    component: ModalDialog,
+    argTypes: {
+        onCloseClicked: { action: 'close-clicked' },
+        header: {
+            table: {
+                disable: true,
+            },
+        },
+        body: {
+            table: {
+                disable: true,
+            },
+        },
+        footer: {
+            table: {
+                disable: true,
+            },
+        },
+    },
+} as ComponentMeta<typeof ModalDialog>
+
+const Template: ComponentStory<typeof ModalDialog> = (args) => (
+    <ModalDialog {...args} />
+)
+
+export const Default = Template.bind({})
+Default.args = {
+    header: (
+        <h1 className="m-0 text-xl font-medium leading-normal text-gray-800">
+            Header
+        </h1>
+    ),
+    body: (
+        <>
+            <p>
+                Aperiam animi est ut cupiditate vitae beatae. Dolores
+                consequatur quibusdam natus saepe et iure impedit molestiae. Et
+                aut omnis eum velit.
+            </p>
+
+            <p>
+                Alias veritatis perspiciatis eligendi consequuntur vero ullam.
+                Laborum sunt beatae ipsa. Ut molestias eum adipisci. Excepturi
+                tempore in omnis dolor.
+            </p>
+
+            <p>
+                Amet error repellat non doloremque modi deserunt. Impedit
+                voluptatum optio dolorem odio nisi. Perferendis aliquam omnis
+                eos qui qui error sequi. Nostrum et eius facilis ut ut iusto
+                eveniet. Quasi laborum eos culpa in. Dolor laborum delectus non
+                ut.
+            </p>
+
+            <p>
+                Maiores illo incidunt ipsa. Veniam non sint perspiciatis odit
+                nihil adipisci vero praesentium. Ipsa consequatur distinctio
+                nisi qui omnis architecto.
+            </p>
+
+            <p>
+                Dolor sit culpa quasi quo architecto. Reiciendis soluta facere
+                eum nemo maiores consequatur ipsa. Esse quaerat culpa eligendi
+                sed enim cupiditate adipisci.
+            </p>
+        </>
+    ),
+
+    footer: (
+        <>
+            <button className="bg-qc-header hover:bg-qc-header-second focus:bg-qc-header-second text-white py-3 px-4 rounded-sm">
+                Button 1
+            </button>
+
+            <button className="bg-qc-header hover:bg-qc-header-second focus:bg-qc-header-second text-white py-3 px-4 rounded-sm">
+                Button 2
+            </button>
+        </>
+    ),
+}
