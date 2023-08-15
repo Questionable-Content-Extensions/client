53fd04e5a4ba5b74a85577b1c8eb5cded9346202
Alexander Krivács Schrøder <alexschrod@gmail.com>
2023-08-15 08:03:59 +0200

    fix: only show "Approximately" after a date when there's actually a date to show

--- src/components/Date.tsx
+++ src/components/Date.tsx
@@ -61,7 +61,7 @@ export default function DateComponent() {
                     ) : (
                         <></>
                     )}{' '}
-                    {approximateDate ? '(Approximately)' : <></>}
+                    {date && approximateDate ? '(Approximately)' : <></>}
                 </b>
             </div>
         </div>
