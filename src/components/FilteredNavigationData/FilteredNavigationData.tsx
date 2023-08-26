7fa006eebff66c02efb6c9f84a3d0f8933b73270
Alexander Krivács Schrøder <alexschrod@gmail.com>
2023-08-26 17:45:29 +0200

    feat: filtering by type, resetting filter with middle click and indicating no matches

--- src/components/FilteredNavigationData/FilteredNavigationData.tsx
+++ src/components/FilteredNavigationData/FilteredNavigationData.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 import ItemNavigation from '@components/ComicDetailsPanel/ItemNavigation/ItemNavigation'
 import { NavElementMode } from '@components/NavElement/NavElement'
 import { ComicId } from '@models/ComicId'
 import { HydratedItemNavigationData } from '@models/HydratedItemData'
 import { ItemId } from '@models/ItemId'
+import { ItemType } from '@models/ItemType'
 
 export default function FilteredNavigationData({
     isLoading,
@@ -43,10 +44,13 @@ export default function FilteredNavigationData({
         }
     }, [filter])
 
+    const filteredItemData = useMemo(
+        () => filterItems(itemData, activeFilter),
+        [itemData, activeFilter]
+    )
+
     return (
         <>
-            {/* TODO: Right clicking filter makes it empty and focused(?) */}
-            {/* TODO: Add back support for prefixing ! for cast, @ for location, # for storyline */}
             {/* TODO: Add back support for adding a new item */}
             <input
                 type="text"
@@ -56,22 +60,34 @@ export default function FilteredNavigationData({
                 onChange={(e) => setFilter(e.target.value)}
                 className="w-full border border-qc-header focus:outline-none flex-auto rounded-none pl-2 disabled:opacity-75 mb-2"
                 disabled={isLoading || hasError}
+                onMouseUp={(e) => {
+                    // 1 is middle click, supposedly
+                    if (e.button === 1) {
+                        setFilter('')
+                    }
+                }}
             />
 
-            <div className="overflow-y-auto overflow-x-hidden max-h-52">
-                {/* TODO: Show indication that no items matched the filter(?) */}
-                <ItemNavigation
-                    itemNavigationData={filterItems(itemData, activeFilter)}
-                    isLoading={isLoading}
-                    isFetching={isFetching}
-                    useColors={useColors}
-                    onSetCurrentComic={onSetCurrentComic}
-                    onShowInfoFor={onShowInfoFor}
-                    mode={NavElementMode.Missing}
-                    editMode={editMode}
-                    onAddItem={onAddItem}
-                />
-            </div>
+            {filteredItemData.length > 0 ? (
+                <div className="overflow-y-auto overflow-x-hidden max-h-52">
+                    <ItemNavigation
+                        itemNavigationData={filteredItemData}
+                        isLoading={isLoading}
+                        isFetching={isFetching}
+                        useColors={useColors}
+                        onSetCurrentComic={onSetCurrentComic}
+                        onShowInfoFor={onShowInfoFor}
+                        mode={NavElementMode.Missing}
+                        editMode={editMode}
+                        onAddItem={onAddItem}
+                    />
+                </div>
+            ) : (
+                <p className="italic">
+                    No {getTypeFromFilter(activeFilter)} found matching '
+                    {getFilterWithoutType(activeFilter)}'
+                </p>
+            )}
         </>
     )
 }
@@ -80,9 +96,43 @@ function filterItems(
     allNavigationItems: HydratedItemNavigationData[],
     filter: string
 ) {
-    return allNavigationItems.filter(
+    const type = getTypeFromFilter(filter)
+
+    let filteredByType
+    if (type !== 'item') {
+        filteredByType = allNavigationItems.filter((i) => i.type === type)
+    } else {
+        filteredByType = allNavigationItems
+    }
+
+    filter = getFilterWithoutType(filter)
+    return filteredByType.filter(
         (i) =>
             i.shortName.toUpperCase().indexOf(filter.toUpperCase()) !== -1 ||
             i.name.toUpperCase().indexOf(filter.toUpperCase()) !== -1
     )
+}
+
+function getTypeFromFilter(filter: string) {
+    const typeFilter = filter.charAt(0)
+    let type: ItemType | 'item' = 'item'
+    switch (typeFilter) {
+        case '!':
+            type = 'cast'
+            break
+        case '@':
+            type = 'location'
+            break
+        case '#':
+            type = 'storyline'
+            break
+    }
+    return type
+}
+
+function getFilterWithoutType(filter: string) {
+    if (getTypeFromFilter(filter) !== 'item') {
+        filter = filter.substring(1)
+    }
+    return filter
 }
